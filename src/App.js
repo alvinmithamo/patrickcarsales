f756cca5d1c913a4237a4fdbf8e0261292847c08
alvinmithamo <ngangaalvin100@gmail.com>
2024-06-02 23:53:23 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from 'react';
-import CustomNavbar from './components/CustomNavbar';
-import HeroSection from './components/HeroSection';
-import ExploreCars from './components/ExploreCars';
+import CustomNavbar from './components/CustomNavbar/CustomNavbar';
+import HeroSection from './components/HeroSection/HeroSection';
+import ExploreCars from './components/ExploreCars/ExploreCars';
 import RecentCars from './components/RecentCars';
 
 function App() {
