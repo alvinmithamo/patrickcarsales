a770e156fccc686c37365b142dd1281856db4d89
alvinmithamo <ngangaalvin100@gmail.com>
2024-06-04 15:15:02 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 // import logo from './logo.svg';
 // import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CustomNavbar from './components/CustomNavbar/CustomNavbar';
 import HeroSection from './components/HeroSection/HeroSection';
 import ExploreCars from './components/ExploreCars/ExploreCars';
 import RecentCars from './components/RecentCars';
+import RecentlyAdded from './components/RecentlyAdded';
+import CarListing from './components/CarListing/CarListing';
+import Home from './components/Home';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
-    <div>
-      <CustomNavbar />
-      <HeroSection />
-      <ExploreCars />
-      <RecentCars />
-    </div>
+
+    <Router>
+      <div className='App'>
+        <CustomNavbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element= {<About />} />
+          <Route path='/listing' element= {<CarListing />} />
+          <Route path='/contact-us' element= {<ContactUs />} />
+        </Routes>
+      </div>
+    </Router>
+    // <div className='App'>
+    //   <CustomNavbar />
+    //   <HeroSection />
+    //   <ExploreCars />
+    //   <RecentCars />
+    //   <RecentlyAdded />
+    //   <CarListing />
+
+    //   </div>
+    
+
+      
+      
+  
   );
 }
 
