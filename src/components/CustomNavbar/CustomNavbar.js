768205be32c86b0f3a8dc6c2a82fcae8ae6944b2
alvinmithamo <ngangaalvin100@gmail.com>
2024-06-04 15:16:46 +0300

    added navlinks

--- src/components/CustomNavbar/CustomNavbar.js
+++ src/components/CustomNavbar/CustomNavbar.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import '../CustomNavbar/CustomNavbar.css'
 
 const CustomNavbar = () => {
   return (
+   
+
     <Navbar bg='light' expand='lg'>
-        <Navbar.Brand href='#'>PATRICK CAR SALES</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'>PATRICK CAR SALES</Navbar.Brand>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-                <Nav.Link href='/about'>About</Nav.Link>
-                <Nav.Link href='/listing'>Car Listing</Nav.Link>
-                <Nav.Link href='#'>Car Makes</Nav.Link>
-                <Nav.Link href='/trade-in'>Trade In</Nav.Link>
-                <Nav.Link href='/contact-us'>Contact Us</Nav.Link>
+                <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                <Nav.Link as={Link} to='/about'>About</Nav.Link>
+                <Nav.Link as={Link} to='/listing'>Car Listing</Nav.Link>
+                {/* <Nav.Link href='#'>Car Makes</Nav.Link> */}
+                {/* <Nav.Link href='/trade-in'>Trade In</Nav.Link> */}
+                <Nav.Link as={Link} to='/contact-us'>Contact Us</Nav.Link>
             </Nav>
             <div className='navbar-buttons'>
-            {/* <Button variant='outline-dark'>Login</Button> */}
-            {/* <Button variant='dark' className='ml-2'>Contact Us</Button> */}
+            {/* <Button variant='outline-dark' className='p-8'>Login</Button>
+            <Button variant='dark' className='ml-2'>Contact Us</Button> */}
             </div>
         </Navbar.Collapse>
     </Navbar>
